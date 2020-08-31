e0db767390a4f9247a8e40ea36096d81719ad2c5
Jalon Dodson <jalon.dododson@yahoo.com>
2020-08-30 22:12:57 -0500

    Passing stuff through to my branch

--- src/components/login-form/LoginForm.js
+++ src/components/login-form/LoginForm.js
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
 import ProptTypes from "prop-types";
-import { Loader } from "../loader";
 import "./LoginForm.css";
 
+// material-ui stuff
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
 export const LoginForm = ({ login, loading, error }) => {
+  // material-ui stuff
+  const classes = useStyles();
+  // end of material-ui stuff
   // Not to be confused with "this.setState" in classes
   const [state, setState] = useState({
     username: "",
@@ -12,6 +31,7 @@ export const LoginForm = ({ login, loading, error }) => {
 
   const handleLogin = (event) => {
     event.preventDefault();
+
     login(state);
   };
 
@@ -23,29 +43,63 @@ export const LoginForm = ({ login, loading, error }) => {
 
   return (
     <React.Fragment>
-      <form id="login-form" onSubmit={handleLogin}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          name="username"
-          value={state.username}
-          autoFocus
-          required
-          onChange={handleChange}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={state.password}
-          required
-          onChange={handleChange}
-        />
-        <button type="submit" disabled={loading}>
-          Login
-        </button>
+      <form className={classes.root} id="login-form" onSubmit={handleLogin}>
+        <div id="inputs">
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="component-outlined">Username</InputLabel>
+            <OutlinedInput
+              type="text"
+              name="username"
+              autoFocus
+              required
+              id="component-outlined"
+              value={state.username}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="component-outlined">Password</InputLabel>
+            <OutlinedInput
+              type="password"
+              name="password"
+              required
+              id="component-outlined"
+              value={state.password}
+              onChange={handleChange}
+            />
+          </FormControl>
+        </div>
+        <div id="btns">
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            id="login-btn"
+            disabled={loading}
+          >
+            Login
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            id="register-btn"
+            disabled={loading}
+          >
+            Register
+          </Button>
+        </div>
+        <Typography
+          variant="overline"
+          display="block"
+          id="forgot"
+          className={classes.root}
+        >
+          FORGOT PASSWORD?
+        </Typography>
+        {loading && <CircularProgress id="loading" />}
       </form>
-      {loading && <Loader />}
+
       {error && <p style={{ color: "red" }}>{error.message}</p>}
     </React.Fragment>
   );
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -3,18 +3,84 @@ import ProptTypes from "prop-types";
 import { Link } from "react-router-dom";
 import "./Menu.css";
 
+// material-ui
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import "fontsource-roboto";
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: "right",
+  },
+  main: {
+    flexGrow: 1,
+    textAlign: "left",
+  },
+}));
+
+const textStyle = makeStyles((theme) => ({
+  root: {},
+}));
+
 export const Menu = ({ isAuthenticated, logout }) => {
+  const classes = useStyles();
+  const text = textStyle();
   return (
     <div id="menu">
-      <h1>Kwitter</h1>
-      {isAuthenticated && (
-        <div id="menu-links">
-          <Link to="/messagefeed">Message Feed</Link>
-          <Link to="/" onClick={logout}>
-            Logout
-          </Link>
-        </div>
-      )}
+      <div className={classes.root}>
+        <Typography variant="h6" id="Kwittext" className={classes.main}>
+          Kwitter
+        </Typography>
+        <Typography
+          variant="overline"
+          display="block"
+          id="simplicity"
+          className={text.root}
+        >
+          Beautiful. Simplistic. Revolutionary. Kwitter.
+        </Typography>
+        {isAuthenticated && (
+          <>
+            <AppBar position="static" color="inherit" id="bar">
+              <Toolbar>
+                <IconButton
+                  edge="start"
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="menu"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Link to="/messagefeed" id="link">
+                  <Typography variant="h6" className={classes.title}>
+                    Feed
+                  </Typography>
+                </Link>
+                <Button
+                  id="logout-btn"
+                  color="inherit"
+                  href="/"
+                  onClick={logout}
+                >
+                  <h3>Log Out</h3>
+                </Button>
+              </Toolbar>
+            </AppBar>
+          </>
+        )}
+      </div>
     </div>
   );
 };
