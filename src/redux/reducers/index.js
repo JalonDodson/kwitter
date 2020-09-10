3ad11d95682a6358a518978288589d4a6e3f5453
Chip Pickering <mykeyworks.2113@gmail.com>
2020-09-10 14:08:08 -0400

    boilerplate for YouTube Player functionality

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,5 +2,11 @@ import { combineReducers } from "redux";
 import { authReducer } from "./auth";
 import { usersReducer } from "./users";
 import { messagesReducer } from "./messages";
+import { playerReducer } from "./player";
 
-export default combineReducers({ auth: authReducer, users: usersReducer, messages: messagesReducer, });
+export default combineReducers({ 
+    auth: authReducer,
+    users: usersReducer,
+    messages: messagesReducer,
+    player: playerReducer,
+});
---
+++ src/utils/youtube-api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const api_key = "";
+class YOUTUBE {
+    axiosInstance = null;
+    
+    constructor() {
+        const axiosInstance = axios.create({
+            baseURL: 'https://www.googleapis.com/youtube/v3/',
+            timeout: 15000,
+            headers: {},
+        });
+
+    };
+};
+export default new YOUTUBE();
