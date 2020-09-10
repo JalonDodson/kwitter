37c942c63da47b59dc420935bc9bd44fcbb5c513
Jalon Dodson <jalon.dododson@yahoo.com>
2020-09-10 14:38:44 -0500

    Making more headway with Google OAuth

---
+++ src/components/GoogleAuth/GoogleAuth.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+
+const CLIENT_ID =
+  "581039245535-affjqmpscsb62d7f6pgkf9ij509bnsu0.apps.googleusercontent.com";
+
+export const GoogleAuth = () => {
+  const [isLogged, setLogged] = useState(false);
+  const [accessToken, setToken] = useState("");
+
+  const login = (res) => {
+    if (res.accessToken) {
+      setLogged(true);
+      setToken(res.accessToken);
+    }
+  };
+
+  const logout = () => {
+      setLogged(false);
+      setToken("");
+  }
+
+  const handleLogoutFail = (res) => {
+      console.table("Google Logout failed", res);
+  }
+
+  const handleLoginFail = (res) => {
+      console.table("Google Login failed.", res)
+  }
+
+  return (
+    <>
+      {isLogged ? (
+        <GoogleLogout
+          clientId={CLIENT_ID}
+          buttonText="Sign Out"
+          onLogoutSuccess={logout}
+          onFailure={handleLogoutFail}
+        />
+      ) : (
+        <GoogleLogin
+          clientId={CLIENT_ID}
+          buttonText="Sign In"
+          onSuccess={login}
+          onFailure={handleLoginFail}
+          cookiePolicy={"single_host_origin"}
+          responseType="code,token"
+        />
+      )}
+      {accessToken ? console.log(`Access Token: ${accessToken}`) : null}
+    </>
+  );
+};
--- src/components/login-form/LoginForm.js
+++ src/components/login-form/LoginForm.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import ProptTypes from "prop-types";
 import "./LoginForm.css";
-// import { google } from "googleapis";
-// material-ui stuff
+import { GoogleAuth } from "../GoogleAuth/GoogleAuth";
+
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
@@ -66,12 +66,12 @@ export const LoginForm = ({ login, loading, error, register, getUser }) => {
     }
   };
 
-  function onSignIn(googleUser) {
-    let profile = googleUser.getBasicProfile();
-    console.log(profile.getId());
-    console.log(profile.getImageUrl());
-    console.log(profile.getName());
-  }
+  // function onSignIn(googleUser) {
+  //   let profile = googleUser.getBasicProfile();
+  //   console.log(profile.getId());
+  //   console.log(profile.getImageUrl());
+  //   console.log(profile.getName());
+  // }
 
   const handleRegister = (event) => {
     event.preventDefault();
@@ -174,7 +174,7 @@ export const LoginForm = ({ login, loading, error, register, getUser }) => {
           >
             Register
           </Button>
-          <Button className="g-signin2" data-onsuccess="onSignIn" />
+          <GoogleAuth />
         </div>
         <Typography
           variant="overline"
